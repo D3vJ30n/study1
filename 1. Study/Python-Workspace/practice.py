814a52e22e333aaddd2f138b3eeb5f9443e82a6e
D3F <jxxdxmxx@naver.com>
2024-07-28 23:33:06 +0900

    2407281133

---
+++ 1. Study/Python-Workspace/practice.py
@@ -0,0 +1,18 @@
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*8)
+print(3*(3+1))
+
+print('풍선')
+print("나비")
+print('zzzzzzzzzzzzzzzzzz')
+
+print("5 + 3 = ", 5 + 3)
+print("2 * 8 = ", 2 * 8)
+print("3 * (3 + 1) = ", 3 * (3 + 1))
+
+print("5 + 3 = " + str(5 + 3) + "2 * 8 = " + str(2 * 8) + "3 * (3 + 1) = " + str(3 * (3 + 1)))
+
